89de626ff847e5a79403294ac5e1468110d1d58e
kinser86 <kinser86@gmail.com>
2017-12-14 20:01:47 -0600

    Create average_out.py

---
+++ Python/average_out.py
@@ -0,0 +1,31 @@
+#Filename: average_output.py
+#Written by: Ryan Kinsey
+#Description: The purpose of this program is to read table 'averagedata' from database 'master_test1.db' and output information into a csv file for evaluation
+
+#Library Imports
+import sqlite3		#This module provides an SQL interface
+import csv		#This module implements classes to read/write to csv files
+
+#Create a connection to the database
+connection = sqlite3.connect('/home/sims/Database/master_test1.db')
+cursor = connection.cursor()
+
+#Notes
+#1 hour = 60 datapoints
+
+#List the database contents
+cursor.execute("SELECT * FROM averagedata ORDER BY key DESC LIMIT 180")
+recordset = cursor.fetchall()
+
+#Declare CSV Writer, filename, and write permissions
+csvWriter = csv.writer(open("/home/sims/CSV/average_output_04102017.csv", "w"))
+
+#Loop through the database and export the data row by row
+for (key, timestamp, val0, val1, val2, val3, average, state) in recordset:
+    print key,timestamp,val0,val1,val2,val3,average,state
+csvWriter.writerows(recordset)
+
+#Commit the transaction and close the connection
+connection.commit()
+cursor.close()
+connection.close()
