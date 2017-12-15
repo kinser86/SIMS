d7cf6f0acedeca221e97a6971dfe9de2c06f6994
kinser86 <kinser86@gmail.com>
2017-12-14 20:00:20 -0600

    Create launch.sh

---
+++ Scripts/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Program Written by: Ryan Kinsey
+#Date: March 22, 2017
+#Filename: launch.sh
+#Purpose: Navigate to specified directory then execute python files.
+
+sudo python /home/sims/Python/read_test.py &
+sudo python /home/sims/Python/state_test.py &
